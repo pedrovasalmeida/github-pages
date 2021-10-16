acecf0350a49a60416c31ac66d077655e17b84e9
pedrovasalmeida <pedrovasalmeida@users.noreply.github.com>
2021-10-16 19:30:53 +0000

    deploy: 485e90f6cc15ad9114fe332f1360dd42b65a86a5

---
+++ static/js/main.29b4469b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{9:function(t,e,o){"use strict";o.r(e);var r=o(1),n=o.n(r),c=o(3),i=o.n(c),s=o(0);var d=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"Portf\xf3lio"})})};i.a.render(Object(s.jsx)(n.a.StrictMode,{children:Object(s.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.29b4469b.chunk.js.map
