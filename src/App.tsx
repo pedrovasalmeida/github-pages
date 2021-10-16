b549634104423e5b9d8bfff1d0232c94ed95ce3f
Pedro Henrique <pedrovasalmeida@gmail.com>
2021-10-16 16:28:19 -0300

    copying old portfolio html and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
+import './styles/global.css';
+import './styles/home.css';
+
 function App() {
-  return (
-    <div>
-      <h1>Portfólio</h1>
-    </div>
-  );
+	return (
+		<div className="container">
+			<span className="message">Soon...</span>
+
+			<section className="made-by">
+				<span className="powered-by">Powered by</span>
+				<span className="my-name">Pedro Henrique de Vasconcellos Almeida</span>
+				<span className="year-copy">2021 &copy;</span>
+
+				<div className="icons">
+					<a
+						href="https://www.linkedin.com/in/pedrovasalmeida"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						<img className="icon" src="./assets/linkedin.svg" alt="Linkedin icon" />
+					</a>
+					<a
+						href="https://github.com/pedrovasalmeida"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						<img className="icon" src="./assets/github.svg" alt="Github icon" />
+					</a>
+					<a
+						className="last"
+						href="https://instagram.com/pedroohva"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						<img className="icon" src="./assets/instagram.svg" alt="Instagram icon" />
+					</a>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
